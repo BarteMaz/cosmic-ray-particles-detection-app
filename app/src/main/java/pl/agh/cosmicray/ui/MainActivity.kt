9ab670a0d50176cacb6eac625f6670b95d58eb98
Katrinn05 <katrin.naumovich@gmail.com>
2025-11-06 23:28:55 +0100

    Refactor repo

--- app/src/main/java/pl/agh/cosmicray/ui/MainActivity.kt
+++ app/src/main/java/pl/agh/cosmicray/ui/MainActivity.kt
@@ -1,10 +1,8 @@
-package pl.agh.cosmicray
+package pl.agh.cosmicray.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import pl.agh.cosmicray.R
 
 class MainActivity : AppCompatActivity() {
 
